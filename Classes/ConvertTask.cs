4ebe69b3eed3301d81c9390e10d6ca6b1bbeb6d7
Seji64 <seji@tihoda.de>
2021-07-24 10:50:23 +0200

    Invalid spacing around the semicolon.

--- Classes/ConvertTask.cs
+++ Classes/ConvertTask.cs
@@ -6,11 +6,11 @@ namespace GPXRide.Classes
 {
     public class ConvertTask{
     
-        public int? Id {get; set;} = null;
-        public GpxFile OriginalGpxFile {get; set;} = null;
-        public string FileName {get; set;}
-        public ItineraryFile ConvertedItineraryFile {get; set;} = null;
-        public ConvertState State {get; set;} = ConvertState.None;
+        public int? Id {get; set; } = null;
+        public GpxFile OriginalGpxFile {get; set; } = null;
+        public string FileName {get; set; }
+        public ItineraryFile ConvertedItineraryFile {get; set; } = null;
+        public ConvertState State {get; set; } = ConvertState.None;
         public ConvertOptions ConvertOptions {get; set; } = new();
         public MudChip SelectedSourceChip { get; set; }
         public SourceType SourceType { get; set; }
--- Classes/Preferences.cs
+++ Classes/Preferences.cs
@@ -5,21 +5,21 @@ namespace GPXRide.Classes
     public class Preferences
     {
         [JsonProperty("motorway")]
-        public bool Motorway {get; set;}
+        public bool Motorway {get; set; }
 
         [JsonProperty("tunnel")]
-        public bool Tunnel {get; set;}
+        public bool Tunnel {get; set; }
 
         [JsonProperty("dirtRoads")]
-        public bool DirtRoads {get; set;}
+        public bool DirtRoads {get; set; }
 
         [JsonProperty("trains")]
-        public bool Trains {get; set;}
+        public bool Trains {get; set; }
 
         [JsonProperty("tollFree")]
-        public bool TollFree {get; set;}
+        public bool TollFree {get; set; }
 
         [JsonProperty("ferry")]
-        public bool Ferry {get; set;}
+        public bool Ferry {get; set; }
     }
 }
