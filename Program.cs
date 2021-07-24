6e4e15369e99f8ef18ca9f0c0a0ad7bce9fef880
Seji64 <seji@tihoda.de>
2021-07-24 10:45:59 +0200

    An opening curly bracket must not be followed by a blank line

--- Program.cs
+++ Program.cs
@@ -13,7 +13,6 @@ namespace GPXRide
     {
         public static async Task Main(string[] args)
         {
-
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
                 .WriteTo.BrowserConsole()
