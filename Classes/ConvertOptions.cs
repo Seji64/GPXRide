ec47db560d96ee9973f2e2db14d3017932a430a2
Seji64 <seji@tihoda.de>
2021-07-24 10:44:41 +0200

    Incorrect spacing around ;

--- Classes/ConvertOptions.cs
+++ Classes/ConvertOptions.cs
@@ -1,15 +1,13 @@
 namespace GPXRide.Classes
 {
     public class ConvertOptions{
-
-        public bool Motorway {get; set;} = true;
-        public bool TollFree {get; set;} = false;
-        public bool DirtyRoads {get; set;} = false;
-        public bool Tunnel {get; set;} = true;
-        public bool Trains {get; set;} = true;
-        public bool Ferry {get; set;} = true;
-        public string RouteName {get; set;}
-        
+        public bool Motorway {get; set; } = true;
+        public bool TollFree {get; set; } = false;
+        public bool DirtyRoads {get; set; } = false;
+        public bool Tunnel {get; set; } = true;
+        public bool Trains {get; set; } = true;
+        public bool Ferry {get; set; } = true;
+        public string RouteName {get; set; }
         public bool FirstWaypointAsMyPosition { get; set; } = false;
     }
 }
