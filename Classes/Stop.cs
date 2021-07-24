f9bf8025e014b8ab506876c8a469a822b75cf179
Seji64 <seji@tihoda.de>
2021-07-24 10:47:37 +0200

    Invalid spacing around the semicolon.

--- Classes/Stop.cs
+++ Classes/Stop.cs
@@ -6,21 +6,21 @@ namespace GPXRide.Classes
     public class Stop
     {
         [JsonProperty("date")]
-        public DateTime Date {get; set;} = DateTime.Now;
+        public DateTime Date {get; set; } = DateTime.Now;
 
         [JsonProperty("isMyPosition")]
-        public bool IsMyPosition {get; set;}
+        public bool IsMyPosition {get; set; }
 
         [JsonProperty("latitude")]
-        public decimal Latitude {get; set;}
+        public decimal Latitude {get; set; }
 
         [JsonProperty("city")]
-        public string City {get; set;}
+        public string City {get; set; }
 
         [JsonProperty("longitude")]
-        public decimal Longitude {get; set;}
+        public decimal Longitude {get; set; }
 
         [JsonProperty("address")]
-        public string Address {get; set;}
+        public string Address {get; set; }
     }
 }
